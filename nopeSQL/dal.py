71b63a83927a2a799c12facce903263ae1a4232a
Chetan Kini <chetan@superconductive.com>
2022-10-11 12:54:54 -0400

    fix: ensure each read/write is a full page

--- nopeSQL/dal.py
+++ nopeSQL/dal.py
@@ -3,68 +3,65 @@ from __future__ import annotations
 import pathlib
 import resource
 from dataclasses import dataclass
-from typing import List
 
 
 @dataclass
 class Page:
-    num: int
-    data: bytes
+    num: int = 0
+    data: bytes = b""
 
 
-@dataclass
 class FreeList:
-    max_page: int
-    freed_pages: List[int]
+    def __init__(self) -> None:
+        self._max_page = 0
+        self._freed_pages = []
 
     def get_next_page(self) -> int:
-        if self.freed_pages:
-            page_id = self.freed_pages.pop()
+        if self._freed_pages:
+            page_id = self._freed_pages.pop()
             return page_id
 
-        self.max_page += 1
-        return self.max_page
+        self._max_page += 1
+        return self._max_page
 
     def free_page(self, page: int) -> None:
-        self.freed_pages.append(page)
+        self._freed_pages.append(page)
 
 
-@dataclass
 class DataAccessLayer:
-    file: pathlib.Path
-    page_size: int
-    free_list: FreeList
+    def __init__(self, file: pathlib.Path, page_size: int) -> None:
+        self._file = file
+        self._page_size = page_size
+        self._free_list = FreeList()
 
     @classmethod
     def create(
         cls, path: str = "nopesql.db", page_size=resource.getpagesize()
     ) -> DataAccessLayer:
         file = pathlib.Path(path)
-        free_list = FreeList(max_page=0, freed_pages=[])
-        return cls(file=file, page_size=page_size, free_list=free_list)
+        return cls(file=file, page_size=page_size)
 
     def read_page(self, page_num: int) -> Page:
-        page = self.allocate_empty_page()
-
-        offset = page_num * self.page_size
-        with open(self.file, "rb") as f:
+        offset = page_num * self._page_size
+        with open(self._file, "rb") as f:
             f.seek(offset)
-            data = f.readline()
+            data = f.read(self._page_size)
 
-        page.data = data
-        return page
+        return Page(num=page_num, data=data)
 
     def write_page(self, page: Page) -> None:
-        offset = page.num * self.page_size
-        with open(self.file, "ab") as f:
-            f.seek(offset)
-            f.write(page.data)
+        offset = page.num * self._page_size
 
-    def allocate_empty_page(self) -> Page:
-        return Page(num=0, data=b"")
+        data = page.data
+        padding = offset - len(data)
+        payload = data.ljust(padding, b"\0")
+
+        with open(self._file, "ab") as f:
+            f.seek(offset)
+            f.write(payload)
 
     def get_next_page(self) -> int:
-        return self.free_list.get_next_page()
+        return self._free_list.get_next_page()
 
     def free_page(self, page: int) -> None:
-        self.free_list.free_page(page)
+        self._free_list.free_page(page)
